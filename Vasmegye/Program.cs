31926945dc396888468cca2dcf7e4e74ee375be3
cisco <cisco@DESKTOP-D4D1G54>
2022-10-10 09:06:49 +0200

    Add project files.

---
+++ Vasmegye/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Vasmegye
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader str = new StreamReader("vas.txt");
+            
+            Console.WriteLine("2.feladat: Adatok beolvasása, tárolása.");
+                   
+            Console.WriteLine("4.feladat: Ellenőrzés");
+            Console.WriteLine("\t");
+            Console.WriteLine($"5.feladat: Vas megyében a vizsgált évek alatt {adat.Count} csecsemő született.");
+            Console.WriteLine($"6.feladat: Fiúk száma: {}");
+
+            Console.WriteLine("\nProgram vége!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Vasmegye/Vas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vasmegye
+{
+
+    internal class Vas
+    {
+        static List<string> adat = new List<string>();
+    }
+}
